15d60ecea48a6e1cb7f6397e1c6be76dad183872
Konrad Paszek <konrad.paszek@nokia.com>
2023-11-09 15:59:10 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -238,6 +238,15 @@ REST_FRAMEWORK = {
         "user_sustained": "5000/day",
         "user_burst": "100/minute",
     },
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter"
+    ],
 }
 SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
